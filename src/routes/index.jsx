13f9a745d07fde5f9120ebc29655749a90a24e2a
Rodolpho Reis <rodolphoreis@live.com>
2024-01-28 12:52:33 +0000

    feat: create Index component

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      Esta é uma demonstração do React Router.
+      <br />
+      Confira{" "}
+      <a href="https://reactrouter.com">os documentos em reactrouter.com</a>.
+    </p>
+  );
+}
