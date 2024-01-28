471cac6744c4bc0c9e939f0d7843f227c7ebc443
Rodolpho Reis <rodolphoreis@live.com>
2024-01-28 12:48:20 +0000

    chore: update main component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import EditContact, { action as editAction } from "./routes/edit";
+import Index from "./routes/index";
+import { loader as rootLoader, action as rootAction } from "./routes/root";
+import {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+import { action as destroyAction } from "./routes/destroy";
+
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
 const router = createBrowserRouter([
@@ -10,10 +19,32 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! Havia um erro.</div>,
+          },
+        ],
       },
     ],
   },
